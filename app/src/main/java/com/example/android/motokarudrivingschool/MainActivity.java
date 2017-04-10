6100a12e1aaae9fd202f6eb3527863601682ecf4
simq <simq@lord.com>
2017-04-10 20:00:53 +0300

    Add e-mail commit

--- app/src/main/java/com/example/android/motokarudrivingschool/MainActivity.java
+++ app/src/main/java/com/example/android/motokarudrivingschool/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.android.motokarudrivingschool;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static com.example.android.motokarudrivingschool.R.array.emails;
+import static com.example.android.motokarudrivingschool.R.string.email;
+import static com.example.android.motokarudrivingschool.R.string.motokaru;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openEmail(View view){
+
+        Resources res = getResources();
+        String[] recipients = res.getStringArray(R.array.emails);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, ("A-category Driving School"));
+        intent.putExtra(Intent.EXTRA_EMAIL, (recipients));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
